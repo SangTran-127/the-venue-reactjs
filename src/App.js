f394b114b3930ead05f4037c6d1afb283a7caad8
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-26 01:19:13 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './resources/style.css'
 import Header from './components/header_footer/Header'
 import Footer from './components/header_footer/Footer'
+import { ForumTwoTone } from '@material-ui/icons';
 function App() {
   return (
     <div className="App">
       <Header />
+      
       <div style = {{
         background: "red",
         height: "800px"
@@ -18,6 +20,7 @@ function App() {
         background: "green",
         height: "800px"
       }}></div>
+      <Footer />
     </div>
   );  
 }
--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 function Footer() {
     return(
-        <>
-            
-        </>
+        <footer className = "bck_red">
+            <div>
+                <div className="font_righteous footer_logo_venue">The venue</div>
+                <div className="footer_copyright">Sang Tran 2021 all rights reserved</div>
+            </div>
+        </footer>
     )
 }
 export default Footer
