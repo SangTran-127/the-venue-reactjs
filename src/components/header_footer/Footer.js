d6d9a4fed07f05053a7058d462aca6af880b6d0f
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-26 02:12:01 +0700

    upload

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
         <footer className = "bck_red">
             <div>
                 <div className="font_righteous footer_logo_venue">The venue</div>
-                <div className="footer_copyright">Sang Tran 2021 all rights reserved</div>
+                <div className="footer_copyright">sang_tqs 2021 all rights reserved</div>
             </div>
         </footer>
     )
