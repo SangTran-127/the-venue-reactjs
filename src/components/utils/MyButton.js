65b087c77d0aedee5974f8486f86b83aba33082a
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-30 23:05:09 +0700

    hello

--- src/components/utils/MyButton.js
+++ src/components/utils/MyButton.js
@@ -3,7 +3,10 @@ import Button from '@material-ui/core'
 import TicketIcon from '../../resources/images/icons/ticket.png'
 function MyButton() {
     return (
-        <div>button</div>
-    )
+        <div>
+            <p>hello</p>
+        </div>
+
+        )
 }
 export default MyButton
