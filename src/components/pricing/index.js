13e3d676f9909fab0aab9281aeef607dc3a10dd6
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-06-06 04:08:28 +0700

    upload

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -8,13 +8,51 @@ function Pricing() {
         desc: [
             '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
             '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
-            '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system'
-        ]
+            '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give'
+        ],
+        linkto: [
+            'https://facebook.com',
+            'https://instagram.com',
+            'https://twitter.com'
+        ],
+        delay: [500, 0, 500]
+    }
+    const showBoxes = () => {
+        return (
+            priceState.prices.map((box, index) => {
+                return(
+                    <Zoom key={index} className="pricing_item" delay={priceState.delay[index]}>
+                        <div className="pricing_inner_wrapper">
+                            <div className="pricing_title">
+                                <span>${priceState.prices[index]}</span>
+                                <span>${priceState.position[index]}</span>
+                            </div>
+                            <div className="pricing_description">
+                                {priceState.desc[index]}
+                            </div>
+                            <div className="pricing_buttons">
+                                <MyButton
+                                    text="Purchase"
+                                    style={{
+                                    }}
+                                    link={priceState.linkto[index]}
+                                 />
+                            </div>
+                        </div>
+                    </Zoom>
+                )
+            })
+        )
     }
     return (
-        <>
-            Something
-        </>
+        <div className="bck_black">
+            <div className="center_wrapper pricing_section">
+                <h2>Pricing</h2>
+                <div className="pricing_wrapper">
+                    {showBoxes()}
+                </div>
+            </div>
+        </div>
     )
 }
 export default Pricing
