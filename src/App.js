ee75232269959baaa73e871d71a2b8b8a08642a5
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-06-07 02:45:07 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './resources/style.css'
+import {Element} from 'react-scroll'
 import Header from './components/header_footer/Header'
 import Footer from './components/header_footer/Footer'
 import Featured from './components/featured'
@@ -10,32 +11,25 @@ import Location from './components/location'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <>
-        <Featured />
-      </>   
-      <>
-        <VenueNFO />
-      </>
-      <>
+      <Header/>
+      <Element name="featured">
+        <Featured/>
+      </Element>   
+      <Element name="venuenfo">
+        <VenueNFO/>
+      </Element>
+      <Element name="hightlights">
         <HighLights/>
-      </>
-      <>
+      </Element>
+      <Element name="pricing">
         <Pricing/>
+      </Element>
+      <Element name="location">
+        <Location/>
+      </Element>
+      <>
+        <Footer/>
       </>
-      <div style = {{
-        background: "red",
-        height: "800px"
-      }}></div>
-      <div style = {{
-        background: "blue",
-        height: "800px"
-      }}></div>
-      <div style = {{
-        background: "green",
-        height: "800px"
-      }}></div>
-      <Footer />
     </div>
   );  
 }
--- src/components/header_footer/SlideDrawer.js
+++ src/components/header_footer/SlideDrawer.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import {scroller} from 'react-scroll'
 import { Drawer, List, ListItem} from '@material-ui/core'
 function SlideDrawer(props) {
     const links = [
         {where: 'featured',value: 'To top'},
         {where: 'venuenfo',value: 'Venue NFO'},
         {where: 'hightlights',value: 'Hightlights'},
+        {where: 'pricing', value: 'Pricing'},
         {where: 'location',value: 'Location'}
     ]
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element, {
+            duration: 1500,
+            delay: 100,
+            smooth:true,
+            offset: -80
+        } )
+        props.setOpen(false)
+    }
     const renderItem = (item) => {
         return (
-            <ListItem button onClick={() => alert(item.where)} key={item.where}>
+            <ListItem button onClick={() => scrollToElement(item.where)} key={item.where}>
                     {item.value}
             </ListItem>
         )
